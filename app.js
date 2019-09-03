4c109cad220b6f09216641e7e8aab59f51b8ee6b
Hello Cloud <helloocloud@gmail.com>
2019-09-03 15:41:42 +0800

    Fix TypeError - Class constructor HealthChecker cannot be invoked without 'new'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let health = require('@cloudnative/health-connect');
-let healthcheck = health.HealthChecker();
+let healthcheck = new health.HealthChecker();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
